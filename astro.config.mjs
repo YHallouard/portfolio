c7a54ef82db12f5c270020195cab4855be1a66d3
Yann HALLOUARD <yann.hal@gmail.com>
2025-04-16 17:39:21 +0200

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
+  site: 'https://yannhallouard.com',
   base: '/portfolio',
   integrations: [tailwind()],
   output: 'static'
